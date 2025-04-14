cb009b734615b1ea07d6bff101e999978acb29b6
Melissa21-R <melissa2missa@gmail.com>
2025-04-14 16:22:51 -0700

    Update Monster.js

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -31,13 +31,64 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.monsterContainer = this.add.container(this.bodyX, this.bodyY);
+        my.sprite.body = this.add.sprite(0, 0, "monsterParts", "body_blueB.png");
+        my.sprite.rightFeet = this.add.sprite(80, 130, "monsterParts", "leg_blueA.png");
+        my.sprite.leftFeet = this.add.sprite(-80, 130, "monsterParts", "leg_blueA.png");
+        my.sprite.leftFeet.flipX = true;
+        my.sprite.rightArm = this.add.sprite(120, 30, "monsterParts", "arm_whiteE.png");
+        my.sprite.rightArm.angle += -35;
+    
+        my.sprite.leftArm = this.add.sprite(-120, 30, "monsterParts", "arm_whiteE.png");
+        my.sprite.leftArm.angle += 35;
+        my.sprite.leftArm.flipX = true;
 
-        
+        my.sprite.eye1 = this.add.sprite(0, -20, "monsterParts", "eye_cute_light.png");
+        my.sprite.mouth = this.add.sprite(0, 40, "monsterParts", "mouthA.png");
+
+        my.sprite.horn1 = this.add.sprite(50, -75, "monsterParts", "detail_blue_horn_small.png");
+        my.sprite.horn2 = this.add.sprite(-50, -75, "monsterParts", "detail_blue_horn_small.png");
+        my.sprite.horn2.flipX = true;
+
+        my.sprite.monsterContainer.add(
+            [
+                my.sprite.body,
+                my.sprite.rightFeet,
+                my.sprite.leftFeet,
+                my.sprite.rightArm,
+                my.sprite.leftArm,
+                my.sprite.eye1,
+                my.sprite.mouth,
+                my.sprite.horn1,
+                my.sprite.horn2
+            ]
+        );
+
+        this.input.keyboard.start(); 
+        this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A); 
+        this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
-
     update() {
         let my = this.my;    // create an alias to this.my for readability
+        if(this.sKey.isDown)
+        {
+            my.sprite.mouth.setFrame("mouthA.png"); 
+        }
+        else if(this.fKey.isDown)
+        {
+            my.sprite.mouth.setFrame("mouthB.png"); 
+        }
+
+        if(this.aKey.isDown)
+        {
+            my.sprite.monsterContainer.x -= 7;
+        }
+        else if(this.dKey.isDown)
+        {
+            my.sprite.monsterContainer.x += 7;
+        }
 
        
     }
